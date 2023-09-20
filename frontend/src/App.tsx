54c7e99d2c0546b9a02c131252d325392b6da0cc
Osako_21 <will.goupil@gmail.com>
2023-09-20 09:22:48 -0400

    basic component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,62 +1,11 @@
 import './App.css'
-import { useEffect, useState } from 'react';
 import '@mantine/core/styles.css';
+import Actif from './components/Actif';
 
 function App() {
-  const [emplacement, setEmplacement] = useState(null);
-  const fetchEmplacement = async () => {
-    try {
-      const response = await fetch('http://127.0.0.1:8000/api/emplacement/1', {
-        method: 'GET',
-        // mode: 'no-cors'
-      });
-      console.log(response)
-
-      if (response.type === 'opaque') {
-        // Handle the opaque response here
-        console.error('Received an opaque response');
-        return;
-      }
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-  
-      const data = await response.json();
-      console.log(data)
-      setEmplacement(data);
-      // Handle the user data here
-      console.log(data);
-    } catch (error) {
-      // Handle any errors that occurred during the fetch
-      console.error('Fetch error:', error);
-    }
-  };
-  
-
-  useEffect(() => {
-    fetchEmplacement();
-  }, []);
-  console.log(emplacement);
   return (
     <>
-      <h1>Emplacements</h1>
-      <ul>
-        {emplacement && 
-          <li>{emplacement.name}</li>}
-      </ul>
-      <div className="card">
-        <p>
-         This is an edit by William, and this is a second update
-        </p>
-        <p>
-         This is an edit by Xavier Roy :D
-        </p> <p>
-         This is an edit by Nicolas Boissé wassuppp
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <Actif id={1}/>
     </>
   )
 }

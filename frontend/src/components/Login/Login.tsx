42a0be33ddb67528ecc5c9673590882ef1ff8ca8
xavierroy12 <201551593@cegepsherbrooke.qc.ca>
2023-09-20 12:47:14 -0400

    Fixed some more css related issues

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,34 +1,48 @@
 import {
-  TextInput,
-  PasswordInput,
-  Paper,
-  Title,
-  Container,
-  Group,
-  Button,
-} from '@mantine/core';
+    TextInput,
+    PasswordInput,
+    Checkbox,
+    Anchor,
+    Paper,
+    Title,
+    Text,
+    Container,
+    Group,
+    Button,
+  } from '@mantine/core';
+  import './Login.scss';
 
 
 const Login = () => {
 
 
-  return (
-    <Container size={420} my={40}>
-      <Title className="text-red-900" ta="center" >
-        Bienvenue au système d'inventaire du CSHC
-      </Title>
-      <p className="text-red-900" >test tailwind</p>
-      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Nom d'utilisateur" placeholder="User" required />
-        <PasswordInput label="Mot de passe" placeholder="mdp" required mt="md" />
-        <Group justify="space-between" mt="lg">
-        </Group>
-        <Button fullWidth mt="xl">
-          Se connecter
-        </Button>
-      </Paper>
-    </Container>
-  );
+    return (
+        <Container size={420} my={40}>
+          <Title ta="center" className= 'title'>
+            Welcome back!
+          </Title>
+          <Text c="dimmed" size="sm" ta="center" mt={5}>
+            Do not have an account yet?{' '}
+            <Anchor size="sm" component="button">
+              Create account
+            </Anchor>
+          </Text>
+
+          <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+            <TextInput label="Email" placeholder="you@mantine.dev" required />
+            <PasswordInput label="Password" placeholder="Your password" required mt="md" />
+            <Group justify="space-between" mt="lg">
+              <Checkbox label="Remember me" />
+              <Anchor component="button" size="sm">
+                Forgot password?
+              </Anchor>
+            </Group>
+            <Button fullWidth mt="xl">
+              Sign in
+            </Button>
+          </Paper>
+        </Container>
+      );
 
 }
 

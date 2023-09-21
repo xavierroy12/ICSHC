3fd4a55be249f6d762bd15d34cd63a8bcb47e92f
Osako_21 <will.goupil@gmail.com>
2023-09-21 14:31:26 -0400

    fix import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { MantineProvider, createTheme } from '@mantine/core';
-import { BrowserRouter } from 'react-router-dom';
 
 const theme = createTheme({
   /** Put your mantine theme override here */

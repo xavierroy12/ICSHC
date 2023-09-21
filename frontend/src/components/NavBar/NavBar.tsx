2f3945beab22057d1484a4c45fa15d64e3482fe9
Osako_21 <will.goupil@gmail.com>
2023-09-21 14:15:21 -0400

    add login to navbar

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -5,7 +5,9 @@ const NavBar = () => {
   return (
     <div className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <span className="font-semibold text-xl tracking-tight">ICSHC</span>
+        <Link to={`/`}>
+            <span className="font-semibold text-xl tracking-tight">ICSHC</span>
+        </Link>
       </div>
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div className="text-sm lg:flex-grow">

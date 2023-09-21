acf286e2c78795a4d2b0def5cc662017341dd252
Osako_21 <will.goupil@gmail.com>
2023-09-21 14:22:42 -0400

    add actif route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,16 @@
 import '@mantine/core/styles.css';
 import Layout from './Layout';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Actif from './components/Actif';
 
 function App() {
   return (
       <div className="App">
-
         <Router>
-
           <Layout>
-            {/* <Routes>
-               <Route exact path="/" render={() => <HomePage />} />
-            </Routes> */}
+            <Routes>
+               <Route path="/" element={ <Actif id={1} />} />
+            </Routes>
           </Layout>
         </Router>
       </div>
--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -5,11 +5,12 @@ const NavBar = () => {
   return (
     <div className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <span className="font-semibold text-xl tracking-tight">ICSHC</span>
+        <Link to={`/`}>
+          <span className="font-semibold text-xl tracking-tight">ICSHC</span>
+        </Link>
       </div>
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div className="text-sm lg:flex-grow">
-   
           <Link to={`/dashboard`}>
             <Button variant="link" color="white" className="mr-4">
               Dashboard

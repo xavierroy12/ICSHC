475d54a3f6baa62a970955afe13b8568f4608cea
Osako_21 <will.goupil@gmail.com>
2023-09-24 13:48:48 -0400

    modify actif to work with mantine react table

--- frontend/src/components/Actifs/Actifs.tsx
+++ frontend/src/components/Actifs/Actifs.tsx
@@ -1,102 +1,89 @@
-import { Button, Checkbox, Table, Title } from "@mantine/core";
-import { useEffect, useState } from "react";
-import Filtre from "./Filtre";
-import { Actif,FiltreGeneric } from "./type";
-
-
+import { Button, Title } from "@mantine/core";
+import { useEffect, useState, useMemo } from "react";
+import { Actif, TableState } from "./type";
+import {
+    MantineReactTable,
+    useMantineReactTable,
+    type MRT_ColumnDef,
+  } from 'mantine-react-table';
+import { useNavigate } from "react-router-dom";
 
 const Actifs = () => {
-    const [actifs, setActifs] = useState<Actif[]>([]);
+  const navigate = useNavigate();
 
-    const [etats, setEtats] = useState<FiltreGeneric[]>([]);
-    const [categories, setCategories] = useState<FiltreGeneric[]>([]);
-    const [modeles, setModeles] = useState<FiltreGeneric[]>([]);
-    const [localisations, setLocalisations] = useState<FiltreGeneric[]>([]);
-    const [locataires, setLocataires] = useState<FiltreGeneric[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>();
+  const [actifs, setActifs] = useState<Actif[]>([]);
+  const [tableState, setTableState] = useState<TableState>({
+    filters: {},
+    showColumnFilters: true,
+    sortBy: undefined,
+    sortDirection: undefined,
+    page: 1,
+    pageSize: 10,
+  });
 
-    const [currentEtat, setCurrentEtat] = useState<string>("");
-    const [currentCategorie, setCurrentCategorie] = useState<string>("");
-    const [currentModele, setCurrentModele] = useState<string>("");
-    const [currentLocalisation, setCurrentLocalisation] = useState<string>("");
-    const [currentLocataire, setCurrentLocataire] = useState<string>("");
+  const columns = useMemo<MRT_ColumnDef<Actif>[]>( () =>
+    [
+      { accessorKey: "numero_serie", header: "Numéro de série" , enableColumnFilter: false},
+      { accessorKey: "nom", header: "Nom" , enableColumnFilter: false},
+      { accessorKey: "modele", header: "Modèle", filterVariant: "autocomplete", },
+      { accessorKey: "categorie", header: "Catégorie", filterVariant: "autocomplete" },
+      { accessorKey: "statut", header: "Statut", filterVariant: "autocomplete" },
+      { accessorKey: "proprietaire", header: "Propriétaire", filterVariant: "autocomplete" },
+      { accessorKey: "emplacement", header: "Emplacement", filterVariant: "autocomplete" },
+    ],
+    [],
+  );
+  const table = useMantineReactTable({
+    columns: columns,
+    data: actifs,
+    enableFacetedValues: true,
+    enableRowSelection: true,
+    getRowId: (row) => row.id,
+    initialState: { ...tableState, isLoading: isLoading, showColumnFilters: true , },
+    enableStickyHeader: true,
+    mantineTableContainerProps: { sx: { maxHeight: '500px' } },
+    mantineTableBodyCellProps: ({ cell }) => ({
+      onDoubleClick: () => {
+        navigate('/actif/' + cell.row.id);
+      },
+    }),
+  });
+  
+  const handleFilterChange = (column: string, value: string) => {
+    console.log("filter change", column, value);
+    setTableState((prevState) => ({ ...prevState, filters: { ...prevState.filters, [column]: value } }));
+  };
+  
+  useEffect(() => {   
+    setIsLoading(true); 
+    fetch("http://localhost:8000/api/actifs").then((response) => response.json())
+      .then((values) => {setActifs(values);})
+      .then(() => setIsLoading(false))
+      .catch((error) => console.error(error));
 
-    useEffect(() => {
-        
-        fetch("http://localhost:8000/api/etats")
-            .then((response) => response.json())
-            .then((etats) => setEtats(etats));
-        fetch("http://localhost:8000/api/categories")
-            .then((response) => response.json())
-            .then((categories) => setCategories(categories));
-        fetch("http://localhost:8000/api/modeles")
-            .then((response) => response.json())
-            .then((modeles) => setModeles(modeles));
-        fetch("http://localhost:8000/api/emplacements")
-            .then((response) => response.json())
-            .then((localisations) => setLocalisations(localisations));
-        fetch("http://localhost:8000/api/clients")
-            .then((response) => response.json())
-            .then((locataires) => setLocataires(locataires));
-        fetch("http://localhost:8000/api/actifs")
-            .then((response) => response.json())
-            .then((actifs) => {
-                const actifsWithRelations = actifs.map((actif) => {
-                    return {
-                        ...actif,
-                        statut: etats.find((etat) => etat.id === actif.id_statut),
-                        emplacement: localisations.find((localisation) => localisation.id === actif.id_emplacement),
-                        proprietaire: locataires.find((locataire) => locataire.id === actif.id_locataires),
-                    };
-                });
-                setActifs(actifsWithRelations);
-            });
-    }, []);
-    
-    const rows = actifs.map((actif) => (
-        <Table.Tr key={actif.numero_serie}>
-            <Table.Td><Checkbox id={actif.numero_serie} /></Table.Td>
-            <Table.Td>{actif.numero_serie}</Table.Td>
-            <Table.Td>{actif.nom}</Table.Td>
-            <Table.Td>{actif.id_statut}</Table.Td>
-            <Table.Td>{actif.id_modele_commande}</Table.Td>
-            <Table.Td>{actif.id_modele_commande}</Table.Td>
-            <Table.Td>{actif.id_emplacement}</Table.Td>
-            <Table.Td>{actif.id_proprietaire}</Table.Td>
-        </Table.Tr>
-    ));
-    console.log(actifs)
-    
-    return (
-        <div className="w-11/12 mx-auto mt-10">
-            <Title order={1}>Actifs</Title>
-            <div className="flex justify-between">
-               <Filtre values={etats} value={currentEtat} setValue={setCurrentEtat} placeHolder="État"/>
-               <Filtre values={categories} value={currentCategorie} setValue={setCurrentCategorie} placeHolder="Catégorie" />
-               <Filtre values={modeles} value={currentModele} setValue={setCurrentModele} placeHolder="Modèle" />
-               <Filtre values={localisations} value={currentLocalisation} setValue={setCurrentLocalisation} placeHolder="Localisation" />
-               <Filtre values={locataires} value={currentLocataire} setValue={setCurrentLocataire} placeHolder="Asigné à" />
-            </div>
-            <Table className="mt-5 max-h-80" highlightOnHover withTableBorder>
-                <Table.Thead>
-                    <Table.Tr>
-                        <Table.Th>Selectionner</Table.Th>
-                        <Table.Th>Numero Serie</Table.Th>
-                        <Table.Th>Nom</Table.Th>
-                        <Table.Th>État</Table.Th>
-                        <Table.Th>Catégorie</Table.Th>
-                        <Table.Th>Modèle</Table.Th>
-                        <Table.Th>Localisation</Table.Th>
-                        <Table.Th>Assigné à</Table.Th>
-                    </Table.Tr>
-                </Table.Thead>
-                <Table.Tbody>{rows}</Table.Tbody>
-            </Table>
-            <div className="flex justify-between mt-5">
-                <Button className="">Ajouter</Button>
-                <Button className="">Modifier</Button>
-            </div>
-        </div>
-    );
+  }, []);
+  useEffect(() => {
+    // TODO: adapt to filter change
+    console.log("filter change");
+    setTableState((prevState) => ({ ...prevState, filters: { ...prevState.filters, statut: "En stock" } }));
+  }, []);
+
+  return (
+    <div className="w-11/12 mx-auto mt-10">
+      <Title order={1}>Actifs</Title>
+      <div>
+        <MantineReactTable table={table}/>
+      </div>
+      <div className="flex justify-between mt-5">
+        <Button.Group>
+          <Button color="indigo">Ajouter</Button>
+          <Button className="shown visible" color="yellow" disabled={Object.keys(table.getState().rowSelection).length === 0}>Modifier</Button>
+        </Button.Group>
+
+      </div>
+    </div>
+  );
 };
 
 export default Actifs
--- frontend/src/components/Actifs/type.ts
+++ frontend/src/components/Actifs/type.ts
@@ -1,18 +1,24 @@
 export type Actif = {
+    id: string;
     numero_serie: string;
     nom: string;
-    modele_commande: string;
+    modele: string;
+    modele_id: number;
+    categorie: string;
+    categorie_id: number;
     statut: string;
-    emplacement: string;
+    statut_id: number;
     proprietaire: string;
+    proprietaire_id: number;
+    emplacement: string;
+    emplacement_id: number;
 }
-export type FiltreGeneric = {
-    id: number;
-    nom: string;
-}
-export type FiltreProps = {
-    values: FiltreGeneric[];
-    value: string;
-    setValue: (value: string) => void;
-    placeHolder: string;
-};
+
+export type TableState = {
+    filters?: Record<string, string>;
+    showColumnFilters?: boolean;
+    sortBy?: string;
+    sortDirection?: "asc" | "desc";
+    page?: number;
+    pageSize?: number;
+  };

934cbcdf409405c5a875ad81b6df8eb46e1ded67
xavierroy12 <201551593@cegepsherbrooke.qc.ca>
2023-09-28 12:18:49 -0400

    changing login frontend

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,13 +1,11 @@
 import {
     TextInput,
     PasswordInput,
-    Checkbox,
     Anchor,
     Paper,
     Title,
     Text,
     Container,
-    Group,
     Button,
   } from '@mantine/core';
     import { useState } from 'react';
@@ -59,28 +57,15 @@ import {
 
 
     return (
-        <Container size={420} my={40}>
+        <Container>
           <Title ta="center" className= 'title'>
-            Welcome back!
+            Inventaire CSHC !
           </Title>
-          <Text c="dimmed" size="sm" ta="center" mt={5}>
-            Do not have an account yet?{' '}
-            <Anchor size="sm" component="button">
-              Create account
-            </Anchor>
-          </Text>
-
           <Paper withBorder shadow="md" p={30} mt={30} radius="md">
             <TextInput label="username" placeholder="you@mantine.dev" required value={username} onChange={handleUsernameChange} />
             <PasswordInput label="password" placeholder="Your password" required mt="md" value={password} onChange={handlePasswordChange} />
-            <Group justify="space-between" mt="lg">
-              <Checkbox label="Remember me" />
-              <Anchor component="button" size="sm">
-                Forgot password?
-              </Anchor>
-            </Group>
             <Button fullWidth mt="xl" onClick={handleLogin}>
-              Sign in
+              Login
             </Button>
           </Paper>
         </Container>

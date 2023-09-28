cc6aef7be838f547bb9f0fb50da534a0acb16612
xavierroy12 <201551593@cegepsherbrooke.qc.ca>
2023-09-28 09:29:07 -0400

    fix api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmplacementController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UtilisateurController;
 
 /*
 |--------------------------------------------------------------------------

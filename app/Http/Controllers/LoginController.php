9f0cade8558b14e627d6b37c91ea214a9826deb4
xavierroy12 <201551593@cegepsherbrooke.qc.ca>
2023-09-28 08:33:52 -0400

    f

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -28,7 +28,6 @@ class LoginController extends Controller
 
         //Function to show attribute of that DN
         function showattrib($ad, $userdn, $attrib) {
-
             $attributes = array($attrib);
             $result = ldap_read($ad, $userdn, "(objectclass=*)", $attributes);
             if ($result === FALSE) { return FALSE; };
@@ -36,59 +35,25 @@ class LoginController extends Controller
             return $entries[0][$attrib][0];
             }
 
-        function getOUs($userdn) {
-            $ous = array();
-            $dn_parts = ldap_explode_dn($userdn, 0);
-            for ($i = 0; $i < $dn_parts['count']; $i++) {
-                $part = $dn_parts[$i];
-                if (strpos($part, 'OU=') === 0) {
-                    $ou = substr($part, 3);
-                    array_push($ous, $ou);
-                }
-            }
-            return $ous;
-        }
-        function getCN($dn) {
-            $dn_parts = ldap_explode_dn($dn, 0);
-            for ($i = 0; $i < $dn_parts['count']; $i++) {
-                $part = $dn_parts[$i];
-                if (strpos($part, 'CN=') === 0) {
-                    return substr($part, 3);
-                }
-            }
-            return '';
-        }
-
-        
-
+        //Variables for LDAP connection
         $domain = 'cshc.qc.ca';
         $basedn = 'dc=cshc,dc=qc,dc=ca';
         $group = 'TechInfo-GLPI';
 
+        //LDAP connection
         $ad = ldap_connect("ldap://{$domain}");
         ldap_set_option($ad, LDAP_OPT_PROTOCOL_VERSION, 3);
         ldap_set_option($ad, LDAP_OPT_REFERRALS, 0);
+        
         if (@ldap_bind($ad, "{$user}@{$domain}", $password)) {
             error_log("LDAP bind successful for user: $user");
+
             $userdn = getDN($ad, $user, $basedn);
             $result = showattrib($ad, $userdn, "title");
-            $memberof = showattrib($ad, $userdn, "memberof");
-            $ous = getOUs($userdn);
-            $groups = array();
-            foreach ($memberof as $groupdn) {
-                $groupname = getCN($groupdn);
-                array_push($groups, $groupname);
-            }
-
-
             return response()->json([
                 'message' => 'Login Successful',
-                'groups' => $groups,
-                /*'user' => $user,
-
-                'title' => $result,
-
-                'OUs' => $ous*/
+                'user' => $user,
+                'userdn' => $userdn,
             ], 200);
         }
         else {

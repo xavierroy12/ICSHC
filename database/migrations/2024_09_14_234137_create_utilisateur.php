df01f4a9199ef4fe78cef7f67a426e9a5f5f99ef
xavierroy12 <201551593@cegepsherbrooke.qc.ca>
2023-09-30 14:09:37 -0400

    f

--- database/migrations/2024_09_14_234137_create_utilisateur.php
+++ database/migrations/2024_09_14_234137_create_utilisateur.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('nom_utilisateur', 32);
             $table->unsignedBigInteger('id_emplacement')->nullable();
             $table->unsignedBigInteger('id_role')->nullable();
-            $table->string('token', 60)->nullable(); // Add token column
+            $table->string('token', 500)->nullable(); // Add token column
             $table->timestamp('expiration')->nullable(); // Add expiration column
             $table->foreign('id_emplacement')->references('id')->on('emplacement');
             $table->foreign('id_role')->references('id')->on('role');

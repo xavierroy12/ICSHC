d75a0e7c6dbd29c2badbca209bf168f449c97d8a
xavierroy12 <201551593@cegepsherbrooke.qc.ca>
2023-10-01 17:51:12 -0400

    colors

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -73,10 +73,10 @@ import {
           <Paper withBorder shadow="md" p={30} mt={30} radius="md" className="w-96">
             <TextInput label="username" placeholder="you@mantine.dev" required value={username} onChange={handleUsernameChange} />
             <PasswordInput label="password" placeholder="Your password" required mt="md" value={password} onChange={handlePasswordChange} />
-            <Button variant="link" fullWidth mt="xl" className="bg-blue-600 hover:bg-blue-700" onClick={handleLogin}>
+            <Button variant="link" fullWidth mt="xl" className="purpleblue hover:bg-blue-700" onClick={handleLogin}>
               Login
             </Button>
-            <Button variant="link" fullWidth mt="xl" className="bg-blue-600 hover:bg-blue-700" onClick={bypass}>
+            <Button variant="link" fullWidth mt="xl" className="purpleblue hover:bg-blue-700" onClick={bypass}>
               Bypass
             </Button>
           </Paper>

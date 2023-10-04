00bc40370c46c174dddab3d90b74052875cee519
Osako_21 <will.goupil@gmail.com>
2023-10-04 10:08:07 -0400

    fix actif form

--- frontend/src/components/SingleActif/ActifForm.tsx
+++ frontend/src/components/SingleActif/ActifForm.tsx
@@ -1,14 +1,15 @@
-import { Form, useForm } from '@mantine/form';
+import { Form, useForm, isNotEmpty } from '@mantine/form';
 import { ActifFormValues, Actif_Type } from './type';
 import {
   Button,
   Checkbox,
-  Select,
   TextInput,
   Textarea,
   SelectItem,
+  NativeSelect,
 } from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
+import { DateInput } from '@mantine/dates';
 
 type Props = {
   id: string;
@@ -33,7 +34,7 @@ const ActifForm = ({
   proprietaires,
 }: Props) => {
   const navigate = useNavigate();
-
+  console.log(actif);
   const form = useForm<ActifFormValues>({
     initialValues: {
       numero_serie: actif.numero_serie,
@@ -51,69 +52,60 @@ const ActifForm = ({
       date_retour: actif.date_retour,
       note: actif.note,
     },
-    // validate: {
-    //   numero_serie: (value: string | undefined) => null,
-    //   nom: (value: string | undefined) => null,
-    //   adresse_mac: (value: string | undefined) => null,
-    //   modele: (value: string | undefined) =>
-    //     value ? 'Veuillez entrer un modèle' : null,
-    //   categorie: (value: string | undefined) =>
-    //     value ? 'Veuillez entrer une catégorie' : null,
-    //   assigne_a: (value: string | undefined) => null,
-    //   emplacement: (value: string | undefined) =>
-    //     value ? 'Veuillez entrer un emplacement' : null,
-    //   statut: (value: string | undefined) =>
-    //     value ? 'Veuillez entrer un statut' : null,
-    //   proprietaire: (value: string | undefined) =>
-    //     value ? 'Veuillez entrer un propriétaire' : null,
-    //   utilisation: (value: string | undefined) =>
-    //     value ? 'Veuillez entrer une utilisation' : null,
-    //   date_creation: (value: string | undefined) => null,
-    //   est_en_entrepot: (value: boolean | undefined) => null,
-    //   date_retour: (value: string | undefined) => null,
-    //   note: (value: string | undefined) => null,
-    // },
+    validate: {
+      numero_serie: isNotEmpty('Veuillez entrer un numéro de série'),
+      nom: isNotEmpty('Veuillez entrer un nom'),
+      adresse_mac: isNotEmpty('Veuillez entrer une adresse MAC'),
+      id_statut: isNotEmpty('Veuillez entrer un statut'),
+      id_emplacement: isNotEmpty('Veuillez entrer un emplacement'),
+      id_proprietaire: isNotEmpty('Veuillez entrer un propriétaire'),
+      id_utilisation: isNotEmpty('Veuillez entrer une utilisation'),
+      id_categorie: isNotEmpty('Veuillez entrer une catégorie'),
+      id_modele: isNotEmpty('Veuillez entrer un modèle'),
+      id_assigne_a: isNotEmpty('Veuillez entrer un locataire'),
+    },
   });
-
   const handleSauvegarde = async () => {
-    try {
-      // Map the form values to match the expected field names in your Laravel API
-      const updatedData = {
-        id_categorie: form.values.id_categorie,
-        est_en_entrepot: form.values.est_en_entrepot,
-        date_retour: form.values.date_retour,
-        note: form.values.note,
-        id_assigne_a: form.values.id_assigne_a,
-        id_modele: form.values.id_modele,
-        id_statut: form.values.id_statut,
-        id_emplacement: form.values.id_emplacement,
-        id_proprietaire: form.values.id_proprietaire,
-        id_utilisation: form.values.id_utilisation,
-      };
+    if (form.isDirty()) {
+      try {
+        // Map the form values to match the expected field names in your Laravel API
+        const updatedData = {
+          id_categorie: form.values.id_categorie,
+          est_en_entrepot: form.values.est_en_entrepot,
+          date_retour: form.values.date_retour,
+          note: form.values.note,
+          id_assigne_a: form.values.id_assigne_a,
+          id_modele: form.values.id_modele,
+          id_statut: form.values.id_statut,
+          id_emplacement: form.values.id_emplacement,
+          id_proprietaire: form.values.id_proprietaire,
+          id_utilisation: form.values.id_utilisation,
+        };
 
-      // Make an API request to update the data in the database
-      const response = await fetch(`http://localhost:8000/api/actif/${id}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(updatedData), // Send the updated data with the mapped field names
-      });
+        // Make an API request to update the data in the database
+        const response = await fetch(`http://localhost:8000/api/actif/${id}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(updatedData), // Send the updated data with the mapped field names
+        });
 
-      // Check if the API request was successful
-      if (response.ok) {
-        // Display a success message to the user
-        alert('Données sauvegardées avec succès');
-        console.log('Données sauvegardées avec succès: ', updatedData);
-        navigate('/actifs');
-      } else {
+        // Check if the API request was successful
+        if (response.ok) {
+          // Display a success message to the user
+          alert('Données sauvegardées avec succès');
+          console.log('Données sauvegardées avec succès: ', updatedData);
+          navigate('/actifs');
+        } else {
+          // Handle errors if the API request fails
+          console.error('Error saving data:', response.statusText);
+          console.log('CA NE FONCTIONNE PAS ', updatedData);
+        }
+      } catch (error) {
         // Handle errors if the API request fails
-        console.error('Error saving data:', response.statusText);
-        console.log('CA NE FONCTIONNE PAS ', updatedData);
+        console.error('Error saving data:', error);
       }
-    } catch (error) {
-      // Handle errors if the API request fails
-      console.error('Error saving data:', error);
     }
   };
 
@@ -159,57 +151,54 @@ const ActifForm = ({
       </div>
 
       <div className="input-container">
-        <Select
+        <NativeSelect
           label="Modèle :"
           className="input-field"
           placeholder="Veuillez choisir un modèle"
           value={form.values.id_modele}
-          defaultValue={'1'}
+          defaultValue={form.values.id_modele}
           onChange={(value) => {
-            if (value) form.setFieldValue('id_modele', value);
-            console.log('Valeur de modele: ', value);
+            form.setFieldValue('id_modele', value.target.value);
           }}
           data={modeles}
         />
       </div>
 
       <div className="input-container">
-        <Select
+        <NativeSelect
           label="Catégorie :"
           className="input-field"
           placeholder="Veuillez choisir une catégorie"
+          defaultValue={categories[1].value}
           value={form.values.id_categorie}
           onChange={(value) => {
-            if (value) form.setFieldValue('id_categorie', value);
-            console.log('Valeur de categorie: ', value);
+            form.setFieldValue('id_categorie', value.target.value);
           }}
           data={categories}
         />
       </div>
 
       <div className="input-container">
-        <Select
+        <NativeSelect
           label="Assigné à :"
           className="input-field"
           placeholder="Veuillez choisir un locataire"
           value={form.values.id_assigne_a}
           onChange={(value) => {
-            if (value) form.setFieldValue('id_assigne_a', value);
-            console.log('Valeur de locataire: ', value);
+            form.setFieldValue('id_assigne_a', value.target.value);
           }}
           data={locataires}
         />
       </div>
 
       <div className="input-container">
-        <Select
+        <NativeSelect
           label="Emplacement :"
           className="input-field"
           placeholder="Veuillez choisir un emplacement"
           value={form.values.id_emplacement}
           onChange={(value) => {
-            if (value) form.setFieldValue('id_emplacement', value);
-            console.log('Valeur de emplacement: ', value);
+            form.setFieldValue('id_emplacement', value.target.value);
           }}
           data={emplacements}
         />
@@ -222,78 +211,73 @@ const ActifForm = ({
           checked={form.values.est_en_entrepot}
           onChange={(event) => {
             form.setFieldValue('est_en_entrepot', event.currentTarget.checked);
-            console.log(
-              `Valeur de est_en_entrepot: ${event.currentTarget.checked}`
-            );
           }}
         />
       </div>
 
       <div className="input-container">
-        <Select
+        <NativeSelect
           label="Statut :"
           className="input-field"
           placeholder="Veuillez choisir un statut"
           value={form.values.id_statut}
           onChange={(value) => {
-            if (value) form.setFieldValue('id_statut', value);
-            console.log('Valeur de statut: ', value);
+            form.setFieldValue('id_statut', value.target.value);
           }}
           data={statuts}
         />
       </div>
 
       <div className="input-container">
-        <Select
+        <NativeSelect
           label="Propriétaire :"
           className="input-field"
           placeholder="Veuillez choisir un propriétaire"
           value={form.values.id_proprietaire}
           onChange={(value) => {
-            if (value) form.setFieldValue('id_proprietaire', value);
-            console.log('Valeur de proprietaire: ', value);
+            form.setFieldValue('id_proprietaire', value.target.value);
           }}
           data={proprietaires}
         />
       </div>
 
       <div className="input-container">
-        <Select
+        <NativeSelect
           label="Utilisation :"
           className="input-field"
           placeholder="Veuillez choisir une utilisation"
           value={form.values.id_utilisation}
           onChange={(value) => {
-            if (value) form.setFieldValue('id_utilisation', value);
-            console.log('Valeur de utilisation: ', value);
+            form.setFieldValue('id_utilisation', value.target.value);
           }}
           data={utilisations}
         />
       </div>
 
       <div className="input-container">
-        <TextInput
+        <DateInput
           label="Date de création :"
           className="input-field"
           value={
             form.values.date_creation
               ? new Date(form.values.date_creation)
-                  .toISOString()
-                  .substring(0, 10)
-              : ''
+              : undefined
           }
           disabled
         />
       </div>
 
       <div className="input-container">
-        <TextInput
+        <DateInput
           label="Date de retour :"
           className="input-field"
-          value={form.values.date_retour}
-          onChange={(event) => {
-            form.setFieldValue('date_retour', event.currentTarget.value);
-            console.log(`Valeur de date_retour: ${event.currentTarget.value}`);
+          value={
+            form.values.date_retour
+              ? new Date(form.values.date_retour)
+              : undefined
+          }
+          onChange={(value) => {
+            form.setFieldValue('date_retour', value?.toString());
           }}
         />
       </div>
@@ -305,7 +289,6 @@ const ActifForm = ({
           value={form.values.note}
           onChange={(event) => {
             form.setFieldValue('note', event.currentTarget.value);
-            console.log(`Valeur de note: ${event.currentTarget.value}`);
           }}
         />
       </div>
@@ -318,6 +301,7 @@ const ActifForm = ({
           size="md"
           type="submit"
           onClick={handleSauvegarde}
+          disabled={!form.isDirty()}
         >
           Sauvegarder
         </Button>

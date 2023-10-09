4a1b5efa6ae536e9a8630444dfb416b70dc91ef4
Osako_21 <will.goupil@gmail.com>
2023-10-08 21:08:06 -0400

    fix navBar

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -14,46 +13,46 @@ const NavBar = () => {
           </Button>
         </Link>
         <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-            <Button
-              component={Link}
-              to={`/dashboard`}
-              color="inherit"
-              className="mr-4"
-            >
-              Dashboard
-            </Button>
-            <Button
-              component={Link}
-              to={`/actifs`}
-              color="inherit"
-              className="mr-4"
-            >
-              Actifs
-            </Button>
-            <Button
-              component={Link}
-              to={`/modeles`}
-              color="inherit"
-              className="mr-4"
-            >
-              Modeles
-            </Button>
-            <Button
-              component={Link}
-              to={`/commande`}
-              color="inherit"
-              className="mr-4"
-            >
-              Commande
-            </Button>
-            <Button
-              component={Link}
-              to={`/client`}
-              color="inherit"
-              className="mr-4"
-            >
-              Client
-            </Button>
+          <Button
+            component={Link}
+            to={`/dashboard`}
+            color="inherit"
+            className="mr-4"
+          >
+            Dashboard
+          </Button>
+          <Button
+            component={Link}
+            to={`/actifs`}
+            color="inherit"
+            className="mr-4"
+          >
+            Actifs
+          </Button>
+          <Button
+            component={Link}
+            to={`/modeles`}
+            color="inherit"
+            className="mr-4"
+          >
+            Modeles
+          </Button>
+          <Button
+            component={Link}
+            to={`/commande`}
+            color="inherit"
+            className="mr-4"
+          >
+            Commande
+          </Button>
+          <Button
+            component={Link}
+            to={`/client`}
+            color="inherit"
+            className="mr-4"
+          >
+            Client
+          </Button>
         </div>
       </Toolbar>
     </AppBar>

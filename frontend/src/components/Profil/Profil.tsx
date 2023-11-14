e91d39d5b78c9349f698db96b899807655828495
Osako <will.goupil@gmail.com>
2023-11-14 11:17:27 -0500

    Page mon Profil

--- frontend/src/components/Profil/Profil.tsx
+++ frontend/src/components/Profil/Profil.tsx
@@ -1,13 +1,10 @@
 import { Fragment } from 'react';
 import ProfilUtilisateur from '../ProfilUtilisateur/ProfilUtilisateur';
-import { useParams } from 'react-router-dom';
 
-const Utilisateur = () => {
-  const { id } = useParams<{ id: string }>();
-
+const Profil = () => {
+  const id = 0;
   return (
     <Fragment>{id && <ProfilUtilisateur id={id} isProfil={false} />}</Fragment>
   );
 };
-
-export default Utilisateur;
+export default Profil;
